3a014fd592e582dd4e7a624c402e8c7d004e881f
steven s <stevensouppaya@pm.me>
2022-09-26 22:36:34 +0200

    disable debug console

--- keyboards/preonic/keymaps/tenshi002/rules.mk
+++ keyboards/preonic/keymaps/tenshi002/rules.mk
@@ -6,15 +6,14 @@ BOOTLOADER = stm32-dfu
 # Cause the final step to take longer, but should get you a smaller compiled size.
 # This also disables Action Functions, and Action Macros, both of which are deprecated.
 # This will get you the most savings, in most situations.
-LTO_ENABLE = no
+LTO_ENABLE = yes
 
 # Disables some of the functionality that you may not need.
 # But note that extrakeys disables stuff like the media keys and system volume control.
-CONSOLE_ENABLE = yes				# Console for debug
+CONSOLE_ENABLE = no				# Console for debug
 COMMAND_ENABLE = no				# Commands for debug and configuration
 MOUSEKEY_ENABLE = no			# Mouse keys
 EXTRAKEY_ENABLE = yes			# Audio control and System control
-
 
 BOOTMAGIC_ENABLE = no			# Enable Bootmagic Lite
 
